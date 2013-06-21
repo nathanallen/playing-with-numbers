1e0b3bbe9a122203d041303e094496334112612f
nathanallen <thenathanator@googlemail.com>
2013-06-21 09:32:36 -0300

    Update FizzBuzz.rb

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,10 +1,14 @@
 for i in 1..100
-	if i.modulo(3);	#if i%3 == 0;
-	if i.modulo(5);	#if i%5 == 0; 
-		puts "FizzBuzz"; end
-		puts "Fizz";
-	elsif i%5 == 0
+
+	if i.modulo(3);	#or if i%3 == 0;
+		
+		if i.modulo(5);	#or if i%5 == 0; 
+			puts "FizzBuzz";
+		else 	puts "Fizz";
+		end
+		
+	elsif i.modulo(5);
 		puts "Buzz"
-	else puts i
+	else 	puts i
 	end
 end
