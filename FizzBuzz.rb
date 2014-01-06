d299e0953249b4db85df2a9cd3ea139eb0703c09
nathanallen <thenathanator@gmail.com>
2014-01-06 09:23:34 -0800

    refactored solution

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,34 +1,7 @@
 # Solution using print with new line char.
 for i in 1..100
 	output = ""
-
-	if i%3 == 0
-		output << "Fizz"
-	end
-
-	if i%5 == 0
-		output << "Buzz"
-	end
-		
-	print (!output.empty? ? output : i.to_s) << "\n"
-	
+	output << "Fizz" if i%3 == 0
+	output << "Buzz" if i%5 == 0
+	print (output.empty? ? i.to_s : output) << "\n"
 end
-
-# # Solution using nested loops.
-# for i in 1..100
-
-# 	if i%3 == 0
-
-# 		if i%5 == 0
-# 			p "FizzBuzz"
-# 		else
-# 			p "Fizz"
-# 		end
-
-# 	elsif i%5 == 0
-# 		p "Buzz"
-# 	else
-# 		p i
-# 	end
-
-# end
